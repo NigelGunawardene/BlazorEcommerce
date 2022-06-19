a65822e84ce50795b2113c632b43212ba4cf9b22
CyberMuffin <cybermuffinofficial@gmail.com>
2022-06-19 12:52:41 +0530

    testing

---
+++ Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,12 @@
+﻿//namespace BlazorEcommerce.Client.Shared;
+
+//partial class MainLayout : ComponentBase
+//{
+//    [Parameter]
+//    public bool _drawerOpen { get; set; } = true
+
+//    void DrawerToggle()
+//    {
+//        _drawerOpen = !_drawerOpen;
+//    }
+//}
