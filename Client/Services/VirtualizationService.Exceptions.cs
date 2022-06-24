a5b3dfceac65a6a1cbcdf0daaca6a8cc70e55a3d
CyberMuffin <cybermuffinofficial@gmail.com>
2022-06-24 07:42:08 +0530

    USED DELEGATE! moved exception handling to another class. custom exception. tests pass

---
+++ Client/Services/VirtualizationService.Exceptions.cs
@@ -0,0 +1,22 @@
+﻿using BlazorEcommerce.Shared.Exceptions;
+
+namespace BlazorEcommerce.Client.Services;
+
+public partial class VirtualizationService<T>
+{
+    private delegate IQueryable<T> ReturningQueryableFunction<T>();
+
+    private IQueryable<T> TryCatch(ReturningQueryableFunction<T> returningQueryableFunction)
+    {
+        try
+        {
+            return returningQueryableFunction();
+        }
+        catch (Exception ex)
+        {
+            VirtualizationServiceException virtualizationServiceException = new VirtualizationServiceException(ex);
+            throw virtualizationServiceException;
+        }
+    }
+}
+
--- Client/Services/VirtualizationService.cs
+++ Client/Services/VirtualizationService.cs
@@ -3,24 +3,21 @@ using BlazorEcommerce.Shared.Exceptions;
 
 namespace BlazorEcommerce.Client.Services;
 
-public class VirtualizationService<T> : IVirtualizationService<T>
+public partial class VirtualizationService<T> : IVirtualizationService<T>
 {
+
+    //public uint StartAt { get; set; }
+    //public uint pageSize { get; set; }
+
     private readonly IDataSourceBroker<T> dataSourceBroker;
 
     public VirtualizationService(IDataSourceBroker<T> _dataSourceBroker)
     {
         dataSourceBroker = _dataSourceBroker;
     }
-    public IQueryable<T> LoadFirstPage(uint startAt, uint pageSize)
-    {
-        try
+    public IQueryable<T> LoadFirstPage(uint startAt, uint pageSize) =>
+        TryCatch(() =>
         {
             return this.dataSourceBroker.TakeSkip(startAt, pageSize);
-        }
-        catch (Exception ex)
-        {
-            VirtualizationServiceException virtualizationServiceException = new VirtualizationServiceException(ex);
-            throw virtualizationServiceException;
-        }
-    }
+        });
 }
